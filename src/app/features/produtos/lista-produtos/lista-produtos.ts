e04bac20b7477b22fce07c6999f2f0fd3f8a86af
jhennif_thayna <jhennifthayna2@gmail.com>
2026-07-17 19:12:01 -0300

    implementa lógica da lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { signal } from '@angular/core';
+import {computed} from '@angular/core';
+import {PrecoFormatadoPipe} from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
- produtos = [
+ produtos = signal ([
     { 
       nome: 'Teclado Gamer', 
       preco:149.00
@@ -29,8 +32,17 @@ export class ListaProdutos {
       nome: 'Headset Gamer', 
       preco:699.99
     }
-  ];
+  ]);
+  //funcao para exibir o produto selecionado pelo usuario no console
   exibirProduto (nome: string){
-    console.log ('Produto Selecionado: ', nome);
+    console.log ('Produto selecionado: ', nome);
   }
-}
+
+  adicionaProduto(){
+    this.produtos.update(listaAtual => [
+      ...listaAtual, {nome: 'Sony Playstation 5', preco: 1000}
+    ]);
+  }
+  totalProdutos = computed(()) => this.produtos().length;
+
+  
