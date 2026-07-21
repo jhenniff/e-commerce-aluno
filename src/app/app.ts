23a49095011a923e7739e4947d34d053efa4b82a
jhennif_thayna <jhennifthayna2@gmail.com>
2026-07-21 19:59:18 -0300

    refactor: atualiza componente principal da aplicacao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LowerCasePipe } from '@angular/common'; 
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce-aluno');
+  nomeLoja ='Mercado Liso';
 }
