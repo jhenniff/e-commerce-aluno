33592dc076f11c12954f108de669ebcfb65ca8c9
jhennif_thayna <jhennifthayna2@gmail.com>
2026-07-21 20:05:45 -0300

    feat: adicionar funciomalidade de carrinho

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+  //! logica de carrinho virá na proxima aulas
+  //! por ora, apenas exibe a pagina carrinho 
+}
