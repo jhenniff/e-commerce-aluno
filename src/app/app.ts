e0d8794edc39c432535ee8d03d78b970a5c403a9
jhennif_thayna <jhennifthayna2@gmail.com>
2026-07-22 21:35:49 -0300

    feat(app) atualiza logica do componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LowerCasePipe } from '@angular/common'; 
+import { usuarioLogado, login, logout } from './core/auth';
 
 
 
@@ -14,4 +15,7 @@ import { LowerCasePipe } from '@angular/common';
 export class App {
   protected readonly title = signal('e-commerce-aluno');
   nomeLoja ='Mercado Liso';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
