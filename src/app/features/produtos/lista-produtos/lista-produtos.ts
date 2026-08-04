e2a829a6f274d7a1abce58cfb9787bbe1e531085
jhennif_thayna <jhennifthayna2@gmail.com>
2026-08-03 21:25:22 -0300

    feat:atualiza lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -14,6 +14,7 @@ import { produtosService } from '../produtos.service';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  erro = signal <string | null>(null);
   
 produtos = signal <{nome: string; preco: number}[]>([]);
 
