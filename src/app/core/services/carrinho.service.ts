5586f095b80f27f2c04a9f9821c5424e7221372e
jhennif_thayna <jhennifthayna2@gmail.com>
2026-08-07 21:18:44 -0300

    feat: adiciona servicos do sistema

---
+++ src/app/core/services/carrinho.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import{ signal } from "@angular/core";
+import { computed } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class CarrinhoService {
+
+    private carrinho = signal<{nome: string; preco: number}[]>([]);
+    //? selecao
+    itens = computed(() => this.carrinho());
+
+    quantidadeItens = computed(() => this.carrinho ().length);
+    totalItens = computed(() =>
+    this.carrinho().reduce((total, item) => total + item.preco,0));
+
+    //todo: acoes adicionar produto
+    adicionar(produtos: {nome: string; preco: number;}){
+        this.carrinho.update (lista => [
+            ...lista, produtos]);
+    }
+    //toda acoes de liampeza
+    limpar(){
+        this.carrinho.set([]);
+    } 
+}
