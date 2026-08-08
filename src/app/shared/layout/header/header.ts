31da44f1845d4b8f29ff0c227e9966201a95abc5
jhennif_thayna <jhennifthayna2@gmail.com>
2026-08-07 21:17:43 -0300

    feat: atualiza componente do header

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +12,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header {
-  nomeLoja = 'Mercado Liso'
+  nomeLoja = 'Mercado Liso';
+
+  private carrinhoService = inject(CarrinhoService);
+
+  quantidadeHeader = this.carrinhoService.quantidadeItens;
 }
