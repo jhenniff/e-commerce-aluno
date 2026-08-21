c1bfa97d8e6f1a7dab6d7270acce49cfc5d9ef0e
lavineesz <lavinealvex@gmail.com>
2026-08-21 19:14:07 -0300

    chore: update server configuration

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,7 +1,12 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, inject } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { Router } from '@angular/router';
+
+export const authGuard = ()=> {
+  const router = inject(Router);
+}
 
 const serverConfig: ApplicationConfig = {
   providers: [
