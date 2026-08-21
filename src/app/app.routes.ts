1988368a1127c33580d54a70e5c3910d75669973
lavineesz <lavinealvex@gmail.com>
2026-08-21 19:16:16 -0300

    feat: update application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,63 @@
-import { Routes } from "@angular/router";
-import { authGuard } from "./core/auth.guard";
-import { Checkout } from "./features/checkout/checkout/checkout";
+import { Routes } from '@angular/router';
+import { authGuard } from './core/guard/auth.guard';
+import { adminGuard } from './core/guard/admin.guard';
 
 export const routes: Routes = [
-   {
-    path:'', //! router pare a raiz localhost:4200/
-    loadComponent: () => 
-        import('./features/home/home/home')
-    .then((m) => m.Home),
-   },
-   {
-    path:'produtos',
-    loadComponent: () => 
-        import('./features/produtos/lista-produtos/lista-produtos')
-    .then((m) => m.ListaProdutos),
-   },
-   {
-    path:'carrinho', 
-    canActivate: [ authGuard ],
-    loadComponent: () => 
-        import('./features/carrinho/carrinho/carrinho')
-    .then((m) => m.Carrinho),
-   },
-   {
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home/home').then(
+        (m) => m.Home
+      ),
+  },
 
-    path:'checkout',
-loadComponent:() =>
-    import('./features/checkout/checkout/checkout').then((m)=>Checkout),
+  {
+    path: 'produtos',
+    loadComponent: () =>
+      import(
+        './features/produtos/lista-produtos/lista-produtos'
+      ).then((m) => m.ListaProdutos),
+  },
 
+  {
+    path: 'carrinho',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/carrinho/carrinho/carrinho').then(
+        (m) => m.Carrinho
+      ),
+  },
 
-   },
-   {
+  {
+   path: 'checkout',
+  canActivate: [authGuard],
+    loadComponent: () =>
+     import('./features/checkout/checkout/checkout').then(
+       (m) => m.Checkout
+       ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+     import('./features/login/login/login').then(
+       (m) => m.Login
+      ),
+  },
+
+ {
+     path:'admin',
+    canActivate:[adminGuard],
+      loadComponent:() =>
+      import('./features/admin/admin/admin').then((m) => m.Admin),
+  },
+ {
+  path:'acesso-negado',
+  loadComponent:() =>
+    import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado),
+ },
+
+  {
     path: '**',
     redirectTo: '',
-   },
+  },
 ];
