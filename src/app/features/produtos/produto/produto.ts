e5ee7c235bec8c4b1c89ab04e900f42d44834220
lavineesz <lavinealvex@gmail.com>
2026-08-24 18:54:43 -0300

    feat: update product component

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -1,12 +1,14 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import {MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {ItemCarrinho} from '../../../core/models/item-carrinho';
 
 @Component({
   selector: 'app-produto',
-  imports: [UpperCasePipe, PrecoFormatadoPipe, MatButtonModule, MatCardModule],
+  imports: [ CurrencyPipe, UpperCasePipe, PrecoFormatadoPipe, MatButtonModule, MatCardModule],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
@@ -26,7 +28,7 @@ export class Produto {
   nome: string;
   preco: number;
  }>();
-  adicionarAoCarrinhoo() {
+  adicionarAoCarrinho() {
     this.ProdutoAdicionado.emit({ 
       nome: this.nome, 
       preco: this.preco,
