3a9e828cd962efb80d64e8f93db1fec0d80c7a71
lavineesz <lavinealvex@gmail.com>
2026-08-24 19:00:37 -0300

    feat: add access admin feature

---
+++ src/app/features/admin/admin/admin.ts
@@ -0,0 +1,34 @@
+import { Component, computed, signal, inject } from '@angular/core';
+import { Router } from '@angular/router';
+// import { AuthService } from '../../../core/services/auth.service';
+import{AuthFacade} from '../../../core/facades/auth.facade';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css',
+})
+export class Admin {
+  private router = inject(Router);
+  private authFacade = inject(AuthFacade);
+
+  totalProdutosCadastrados= signal(20);
+  pedidosPendentes = signal (6);
+  usuariosCadastrados= signal(7);
+
+  usuarioAtual = this.authFacade.usuarioAtual;
+
+  areaPerfil = computed(()=>{
+    const usuario =this.usuarioAtual();
+
+    if(!usuario){
+      return 'Nenhum usuario autenticado';
+    }
+    return ' Usuario autenticado como: ${usuario.perfil}.';
+  });
+  sair(){
+    this.authFacade.sair();
+    this.router.navigateByUrl('/login');
+  }
+}
