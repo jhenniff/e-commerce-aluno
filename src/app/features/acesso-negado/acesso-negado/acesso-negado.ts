00f6d4b94c37efc86d33d530f98f2cdc54c39c5f
lavineesz <lavinealvex@gmail.com>
2026-08-24 19:00:07 -0300

    feat: add access danied feature

---
+++ src/app/features/acesso-negado/acesso-negado/acesso-negado.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import{AuthFacade} from '../../../core/facades/auth.facade';
+import { MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-acesso-negado',
+  imports: [RouterLink, MatButtonModule],
+  templateUrl: './acesso-negado.html',
+  styleUrl: './acesso-negado.css',
+})
+export class AcessoNegado {
+  private router = inject(Router);
+  private authFacade = inject(AuthFacade);
+
+  sair(){
+    this.authFacade.sair();
+    this.router.navigateByUrl('/login');
+    return;
+  }
+}
